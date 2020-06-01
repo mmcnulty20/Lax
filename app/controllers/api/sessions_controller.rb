a34f58291db263f4b5d4fcd42e321510afe7eb3a
Megan <megan.mcnulty07@gmail.com>
2020-06-01 16:05:43 -0400

    Finished Auth backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class API::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(user_params[:email], user_params[:password])
+        if @user.save
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: ["Sorry, you entered an incorrect email address or password."], status: 401
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: ["User doesn't exist!"], status: 404
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,8 @@ class User < ApplicationRecord
         SecureRandom.urlsafe_base64(16)
     end
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
         return unless user
         user.is_password?(password) ? user : nil
     end
@@ -39,7 +39,7 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        self.session_token = User.generate_session_token
+        update!(session_token: User.generate_session_token)
         session_token
     end
 
